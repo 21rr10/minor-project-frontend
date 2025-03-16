3e025e2d2bb46cadaf2e4ba6b1bb0eb62c16d1f7
Reet Agarwal <139364881+reetagarwal@users.noreply.github.com>
2025-03-17 00:03:12 +0530

    updated

--- minor-project/src/pages/Home.jsx
+++ minor-project/src/pages/Home.jsx
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-import FlightSearchComponent from "../components/flightSearch";
-function Home(){
-    return(
-       <FlightSearchComponent></FlightSearchComponent>
-=======
 function Home() {
     return (
         <div style={{
@@ -17,12 +11,11 @@ function Home() {
             alignItems: "center",
             color: "white",
             textAlign: "center",
-            paddingTop: "50px"
+            paddingTop: "50px" 
         }}>
             <h1>Welcome to The Trip Finder</h1>
             <p>Start planning your next adventure with us!</p>
         </div>
->>>>>>> Stashed changes
     );
 }
 
