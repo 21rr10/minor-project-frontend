e974ead86a75466d1b741df09af54a2bada1ef81
21rr10 <ranjan.ritesh21102003@gmail.com>
2025-03-16 16:28:59 +0530

    header basic for home

--- minor-project/src/App.jsx
+++ minor-project/src/App.jsx
@@ -3,11 +3,13 @@ import  Signin  from './pages/Signin.jsx';
 import  Signup  from './pages/Signup.jsx';
 // import './App.css';
 import Button from './pages/Account.jsx';
+import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <>
-       <Signin></Signin>
+      <Home></Home>
+       {/* <Signin></Signin> */}
        {/* <Button></Button> */}
        <Signup></Signup>
     </>
---
+++ minor-project/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function Home(){
+    const [searchQuery, setSearchQuery] = useState('');
+    return (
+        <div>
+            <header className="p-4 flex justify-between">
+                <a href="" className="flex item-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 -rotate-90">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                    </svg>
+                    <span className="font-bold text-xl">TravelEase</span>
+                </a>
+
+
+                <div className="flex item-center gap-1">
+                    <div className="flex item-center gap-1">
+                        <input type="text" placeholder="Search..." value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)}></input>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div>DESTINATIONS</div>
+                    <div>EXPERIENCES</div>
+                    <div>INSPIRATIONS</div>
+                    <div>TRIP FINDER</div>
+                    <div>MOST POPULAR</div>
+                    <div>ABOUT US</div>
+                </div>
+                
+            </header>
+        </div>
+    );
+}
+
+export default Home;
