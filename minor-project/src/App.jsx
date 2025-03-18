2cf4b35833c23fe8154316696f267e1751499d5d
21rr10 <ranjan.ritesh21102003@gmail.com>
2025-03-18 13:10:51 +0530

    Travel_weather_flight_hotel_page initial configuration

--- minor-project/src/App.jsx
+++ minor-project/src/App.jsx
@@ -4,14 +4,15 @@ import Header from "./components/header";  // Import Header component
 import "./App.css";
 // import Account from "./pages/Account";
 import Home from "./pages/Home";
+import TravelDetailsPage from "./pages/Travel_deets";
 
 function App() {
   return (
     <div className="App">
-      <Header /> 
+      {/* <Header />  */}
       
-      <Home></Home>
-      
+      {/* <Home></Home> */}
+      <TravelDetailsPage></TravelDetailsPage>
       
       
     </div>
