5ae69b40598f7df983fc830e4043157d600262f2
rishitkamboj <rishit242@gmail.com>
2025-03-29 13:02:53 +0530

    added an error page, started integrating the backend

--- minor-project/src/App.jsx
+++ minor-project/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from "./components/header";  // Import Header component
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
 import "./App.css";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
@@ -9,21 +9,25 @@ import Account from "./pages/Account";
 import MoodSelectionPage from "./pages/selectMood";
 import ItineraryPage from "./pages/itinerary";
 import TravelDetailsPage from "./pages/Travel_deets";
-
+import Check from "./pages/check";
+import ErrorPage from "./pages/ErrorPage"; 
 
 function App() {
   return (
     <Router>
-<Routes>
-  <Route path="/" element={<Home/>}/>
-<Route path="/signup" element={<Signup/>}/>
-<Route path="/signin" element={<Signin/>}/>
-<Route path="/account" element={<Account/>}/>
-<Route path="/moodSelection" element={<MoodSelectionPage/>}/>
-<Route path="/itinerary" element={<ItineraryPage/>}/>
-<Route path="/travelDetails" element={<TravelDetailsPage/>}/>
-</Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/moodSelection" element={<MoodSelectionPage />} />
+        <Route path="/itinerary" element={<ItineraryPage />} />
+        <Route path="/travelDetails" element={<TravelDetailsPage />} />
+        <Route path="/systemcheck" element={<Check />} />
+        <Route path="*" element={<ErrorPage/>} />
+      </Routes>
     </Router>
-  )
+  );
 }
+
 export default App;
